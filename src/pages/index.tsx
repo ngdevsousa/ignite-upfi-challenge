a6f92feee164853995f1d06f9f40362ec9f8a3a4
f.sousa <ng.dev.sousa@outlook.com>
2021-06-02 11:02:58 -0300

    Add getImages with pagination to Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,33 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface GetImageOptions {
+  pageParam?: string;
+}
+
+interface Image {
+  id: string;
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+}
+
+interface ImagesResponse {
+  data: Array<Image>;
+  after: string | null;
+}
+
 export default function Home(): JSX.Element {
+  const getImages = async ({
+    pageParam = '',
+  }: GetImageOptions): Promise<ImagesResponse> => {
+    const { data } = await api.get<ImagesResponse>(
+      `api/images?after=${pageParam}`
+    );
+    return data;
+  };
+
   const {
     data,
     isLoading,
@@ -16,29 +42,34 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', getImages, {
+    getNextPageParam: (prevPage, pages) => prevPage.after,
+  });
 
-  const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+  const formattedData = useMemo<Array<Image>>(() => {
+    return data?.pages.flatMap(page => {
+      return page.data;
+    });
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
-
-  // TODO RENDER ERROR SCREEN
+  if (isError) return <Error />;
 
   return (
     <>
       <Header />
 
-      <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
-      </Box>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Box maxW={1120} px={20} mx="auto" my={20}>
+          <CardList cards={formattedData} />
+          {hasNextPage && (
+            <Button onClick={() => fetchNextPage()}>
+              {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+            </Button>
+          )}
+        </Box>
+      )}
     </>
   );
 }
